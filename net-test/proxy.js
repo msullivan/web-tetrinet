d0b228f02dce36110f2fbe49f7ca06c8b31eba66
Michael Sullivan <sully@msully.net>
2018-02-13 18:16:54 -0800

    reindent proxy.js

--- net-test/proxy.js
+++ net-test/proxy.js
@@ -15,35 +15,35 @@ var carrier = require('carrier');
 
 
 wss.on('connection', function (ws) {
-    var client;
-    ws.on('message', function (msg) {
-        if (!client) {
-            client = net.createConnection(port, host, function () {
-                ws.send("Success!");
-            });
-            // carrier makes it line oriented
-            function process(data) {
-                if (ws.readyState === ws.OPEN) {
-                    ws.send(data, { binary: false });
-                }
-            }
-            carrier.carry(client, process, 'latin1', /\xff|\n/);
-            //client.on('data', process);
-            client.on('error', function(err) {
-                ws.close();
-            });
-            client.on('end', function () {
-                ws.close();
-            });
-        } else {
-            client.write(msg + '\n');
+  var client;
+  ws.on('message', function (msg) {
+    if (!client) {
+      client = net.createConnection(port, host, function () {
+        ws.send("Success!");
+      });
+      // carrier makes it line oriented
+      function process(data) {
+        if (ws.readyState === ws.OPEN) {
+          ws.send(data, { binary: false });
         }
-    });
-    ws.on('close', function () {
-        if (client) {
-            client.end();
-        }
-    });
+      }
+      carrier.carry(client, process, 'latin1', /\xff|\n/);
+      //client.on('data', process);
+      client.on('error', function(err) {
+        ws.close();
+      });
+      client.on('end', function () {
+        ws.close();
+      });
+    } else {
+      client.write(msg + '\n');
+    }
+  });
+  ws.on('close', function () {
+    if (client) {
+      client.end();
+    }
+  });
 });
 
 console.log('Listening on', listenPort);
